1a4a03bcb89d9e2a209042b7b0af8d3dee4bb63d
Yuri Melo <yurisnm@gmail.com>
2023-02-24 16:09:12 -0300

    refact: add some improvements on logging and documentation

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -2,16 +2,29 @@ package com.example.config;
 
 import com.example.wishlist.service.WishlistService;
 import com.example.wishlist.service.WishlistServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for the application.
+ */
 @Configuration
 @ComponentScan("com.example")
 public class AppConfig {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppConfig.class);
+
+    /**
+     * Configures the {@link WishlistService} bean to be used in the application.
+     *
+     * @return The {@link WishlistService} bean.
+     */
     @Bean
     public WishlistService wishlistService() {
+        LOGGER.info("Creating WishlistServiceImpl bean...");
         return new WishlistServiceImpl();
     }
 }
--- src/main/java/com/example/wishlist/service/WishlistServiceImpl.java
+++ src/main/java/com/example/wishlist/service/WishlistServiceImpl.java
@@ -18,14 +18,29 @@ public class WishlistServiceImpl implements WishlistService {
     @Autowired
     private WishlistRepository repository;
 
+    /**
+     * Constructs a new WishlistServiceImpl instance with the provided WishlistRepository.
+     *
+     * @param wishlistRepository the repository to be used to store and retrieve wishlist items.
+     */
     public WishlistServiceImpl(WishlistRepository wishlistRepository) {
         this.repository = wishlistRepository;
     }
 
+    /**
+     * Constructs a new WishlistServiceImpl instance.
+     */
     public WishlistServiceImpl() {
 
     }
 
+    /**
+     * Adds a new item to the wishlist.
+     *
+     * @param item the item to be added to the wishlist.
+     * @return the item that was added to the wishlist.
+     * @throws IllegalStateException if the wishlist already has the maximum number of items allowed.
+     */
     @Override
     public WishlistItem addItem(WishlistItem item) {
         logger.info("Adding item {} to wishlist.", item.getName());
@@ -39,6 +54,12 @@ public class WishlistServiceImpl implements WishlistService {
         return item;
     }
 
+    /**
+     * Removes an item from the wishlist with the given id.
+     *
+     * @param id the id of the item to be removed.
+     * @return true if an item was removed, false otherwise.
+     */
     @Override
     public boolean removeItem(String id) {
         logger.info("Removing item with id {} from wishlist.", id);
@@ -52,18 +73,34 @@ public class WishlistServiceImpl implements WishlistService {
         return exists;
     }
 
+    /**
+     * Retrieves all items from the wishlist.
+     *
+     * @return a list containing all wishlist items.
+     */
     @Override
     public List<WishlistItem> getAllItems() {
         logger.info("Retrieving all items from wishlist.");
         return repository.findAll();
     }
 
+    /**
+     * Retrieves an item from the wishlist with the given id.
+     *
+     * @param id the id of the item to be retrieved.
+     * @return an Optional containing the item with the given id, or an empty Optional if the item was not found.
+     */
     @Override
     public Optional<WishlistItem> getItemById(String id) {
         logger.info("Retrieving item with id {} from wishlist.", id);
         return repository.findById(id);
     }
 
+    /**
+     * Retrieves the number of items in the wishlist.
+     *
+     * @return the number of items in the wishlist.
+     */
     @Override
     public int getItemsCount() {
         int count = (int) repository.count();
